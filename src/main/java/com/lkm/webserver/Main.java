54c8d556b5c745e07d0d67e83851ac2e385f4264
LKM <cnming2@qq.com>
2020-03-15 17:35:09 +0800

    [Server] use muitithread

--- src/main/java/com/lkm/webserver/Main.java
+++ src/main/java/com/lkm/webserver/Main.java
@@ -1,11 +1,17 @@
 package com.lkm.webserver;
 
+import com.lkm.webserver.connection.Connection;
+import com.lkm.webserver.connection.ConnectionPool;
+import com.lkm.webserver.connection.Processor;
+import com.lkm.webserver.constant.Misc;
+
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -23,50 +29,62 @@ class Server {
         try {
             serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.configureBlocking(false);
-            serverSocketChannel.socket().bind(new InetSocketAddress(8080));
+            serverSocketChannel.socket().bind(new InetSocketAddress(Misc.PORT), 1024);
             selector = Selector.open();
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            processRequest();
+            run();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void processRequest() {
+    public void run() {
         while (true) {
             try {
-                selector.select();
+                int n = selector.select(1000);
                 Set<SelectionKey> keys = selector.selectedKeys();
                 Iterator<SelectionKey> iterator = keys.iterator();
                 while (iterator.hasNext()) {
                     SelectionKey key = iterator.next();
                     iterator.remove();
-                    if (key.isAcceptable()) {
-                        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
-                        SocketChannel socketChannel = serverSocketChannel.accept();
-                        socketChannel.configureBlocking(false);
-                        socketChannel.register(selector, SelectionKey.OP_READ);
-                    }
-                    if (key.isReadable()) {
-                        SocketChannel socketChannel = (SocketChannel) key.channel();
-                        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
-                        socketChannel.read(readBuffer);
-                        readBuffer.flip();
-                        System.out.println(new String(readBuffer.array()).trim());
-                        key.interestOps(SelectionKey.OP_WRITE);
-                    }
-                    if (key.isWritable()) {
-                        SocketChannel socketChannel = (SocketChannel) key.channel();
-                        ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
-                        writeBuffer.put("HTTP/1.1 200 OK\r\n\r\n\r\nHELLO WORLD!".getBytes());
-                        writeBuffer.flip();
-                        socketChannel.write(writeBuffer);
-                        socketChannel.close();
+                    if (key.isValid()) {
+                        if (key.isAcceptable()) {
+                            ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                            SocketChannel socketChannel = serverSocketChannel.accept();
+                            socketChannel.configureBlocking(false);
+                            socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                        }
+                        if (key.isReadable()) {
+                            SocketChannel socketChannel = (SocketChannel) key.channel();
+                            int socketChannelHash = socketChannel.hashCode();
+                            ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                            int messageSize = socketChannel.read(readBuffer);
+                            readBuffer.flip();
+                            if (messageSize > 0) {
+                                ConnectionPool.getConnectionPool().putIfAbsent(socketChannelHash,
+                                        new Connection(socketChannel, new ArrayList<>()));
+                                ConnectionPool.getConnectionPool().get(socketChannelHash).updateLastConnectTime();
+
+                                byte[] tmp = new byte[messageSize];
+                                readBuffer.get(tmp);
+                                ConnectionPool.getConnectionPool().get(socketChannelHash)
+                                        .getHttpMessage().add(tmp);
+                                ConnectionPool.getConnectionPool().get(socketChannelHash)
+                                        .addLength(messageSize);
+                            }
+                            if (messageSize < Misc.BUFFER_SIZE) {
+                                Processor.requestExecutor.execute(() -> {
+                                    Processor.processRequest(ConnectionPool.getConnectionPool().get(socketChannelHash));
+                                    ConnectionPool.getConnectionPool().remove(socketChannelHash);
+                                });
+                            }
+                        }
                     }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
         }
     }
 }
