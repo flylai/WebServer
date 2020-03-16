790b92c70e11ded86c2e374875c1f20bcd97485c
LKM <cnming2@qq.com>
2020-03-16 12:47:04 +0800

    [Project] add some misc constants

---
+++ src/main/java/com/lkm/webserver/constant/Misc.java
@@ -0,0 +1,9 @@
+package com.lkm.webserver.constant;
+
+public class Misc {
+    public final static int PORT = 8080;
+    public final static int BUFFER_SIZE = 1024;
+    public final static int EXPIRES_TIME = 60;
+    public final static int MAX_CONNECTION_POOL_SIZE = 100;
+    public final static String CRLF = "\r\n";
+}
