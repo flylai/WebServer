d74bac0e40b7a90a5ccc315c2dc74a5a873aa999
LKM <cnming2@qq.com>
2020-03-19 14:11:38 +0800

    [RequestParser] replace String.split to StringUtil.split for optimization

--- src/main/java/com/lkm/webserver/request/RequestParser.java
+++ src/main/java/com/lkm/webserver/request/RequestParser.java
@@ -2,34 +2,37 @@ package com.lkm.webserver.request;
 
 import com.lkm.webserver.constant.Misc;
 import com.lkm.webserver.constant.RequestMethod;
+import com.lkm.webserver.util.StringUtil;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class RequestParser {
     public static String[] parseMessage(String message) {
         String[] result = new String[3];
         // split header and body
-        String[] tmp = message.split(Misc.CRLF + Misc.CRLF, 2);
+        List<String> tmp = StringUtil.split(message, Misc.CRLF + Misc.CRLF, 3);
         // post body
-        result[2] = tmp[1];
-        String[] lineAndHeader = tmp[0].split(Misc.CRLF, 2);
+        if (tmp.size() > 1) {
+            result[2] = tmp.get(1);
+        }
+        List<String> lineAndHeader = StringUtil.split(tmp.get(0), Misc.CRLF, 2);
         // request line
-        result[0] = lineAndHeader[0];
+        result[0] = lineAndHeader.get(0);
         // headers
-        result[1] = lineAndHeader[1];
+        result[1] = lineAndHeader.get(1);
         return result;
     }
 
     public static RequestLine parseRequestLine(String message) throws Exception {
         HashMap<String, String> query = null;
         RequestMethod method;
-        String path;
 
         // parse request method
-        String[] messageArr = message.split(" ");
-        if ("GET".equals(messageArr[0].toUpperCase())) {
+        List<String> messageArr = StringUtil.split(message, " ");
+        if ("GET".equals(messageArr.get(0).toUpperCase())) {
             method = RequestMethod.GET;
-        } else if ("POST".equals(messageArr[0].toUpperCase())) {
+        } else if ("POST".equals(messageArr.get(0).toUpperCase())) {
             method = RequestMethod.POST;
         } else {
             throw new Exception("Unsupported request method");
@@ -37,34 +40,36 @@ public class RequestParser {
 
         // parse path and query
         // such as /asd/qwe/zxc.html?tyu=x&opq=jkl
-        String url = messageArr[1];
-        String[] pathArr = url.split("\\?");
+        String url = messageArr.get(1);
+        List<String> pathArr = StringUtil.split(url, "?");
         String queryString = "";
 
-        if (pathArr.length > 1) {
-            queryString = pathArr[1];
-            query = parseQueryString(pathArr[1]);
+        String path = pathArr.size() > 0 ? pathArr.get(0) : "/";
+
+        if (pathArr.size() > 1) {
+            queryString = pathArr.get(1);
+            query = parseQueryString(pathArr.get(1));
         }
-        return new RequestLine(query == null ? new HashMap<>() : query, method, pathArr[0], queryString);
+        return new RequestLine(query == null ? new HashMap<>() : query, method, path, queryString);
     }
 
-    public static RequestHeaders parseRequestHeaders(String message) throws Exception {
+    public static RequestHeaders parseRequestHeaders(String message) {
         HashMap<String, String> headers = new HashMap<>();
         HashMap<String, String> cookies = new HashMap<>();
-        String[] headerArr = message.split(Misc.CRLF);
+        List<String> headerArr = StringUtil.split(message, Misc.CRLF);
         for (String header : headerArr) {
-            String[] arg = header.split(":", 2);
-            headers.put(arg[0].trim(), arg[1].trim());
+            List<String> arg = StringUtil.split(header, ":", 2);
+            headers.put(arg.get(0).trim(), arg.get(1).trim());
 
             // cookie: a=1; b=2; c=3
-            if ("cookie".equals(arg[0].trim().toLowerCase())) {
-                String[] cookieArr = arg[1].split(";");
+            if ("cookie".equals(arg.get(0).trim().toLowerCase())) {
+                List<String> cookieArr = StringUtil.split(arg.get(1), ";");
                 for (String cookie : cookieArr) {
                     // a=1
-                    String[] kv = cookie.split("=", 2);
-                    //               a             1
-                    if (kv.length > 1) {
-                        cookies.put(kv[0].trim(), kv[1].trim());
+                    List<String> kv = StringUtil.split(cookie, "=", 2);
+                    if (kv.size() > 1) {
+                        //                 a                 1
+                        cookies.put(kv.get(0).trim(), kv.get(1).trim());
                     }
                 }
             }
@@ -74,12 +79,12 @@ public class RequestParser {
 
     private static HashMap<String, String> parseQueryString(String message) {
         HashMap<String, String> result = new HashMap<>();
-        String[] queryArr = message.split("&");
+        List<String> queryArr = StringUtil.split(message, "&");
         for (String arg : queryArr) {
-            String[] argArr = arg.split("=");
+            List<String> argArr = StringUtil.split(arg, "=");
             // qwe=asd -> key=qwe value=asd
-            if (argArr.length > 1) {
-                result.put(argArr[0], argArr[1]);
+            if (argArr.size() > 1) {
+                result.put(argArr.get(0), argArr.get(1));
             }
         }
         return result;
