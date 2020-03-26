7300fdc1a736f4e7e5f1485b9c149142a1e95567
LKM <cnming2@qq.com>
2020-03-26 13:42:42 +0800

    [Misc] add some constants

--- src/main/java/com/lkm/webserver/constant/Misc.java
+++ src/main/java/com/lkm/webserver/constant/Misc.java
@@ -3,7 +3,17 @@ package com.lkm.webserver.constant;
 public class Misc {
     public final static int PORT = 8080;
     public final static int BUFFER_SIZE = 1024;
-    public final static int EXPIRES_TIME = 60;
+    public final static int EXPIRES_TIME = 5;
     public final static int MAX_CONNECTION_POOL_SIZE = 100;
+    // 10s
+    public final static int BLOCK_TIMEOUT = 10 * 1000;
+    // 1h
+    public final static int SESSION_TIMEOUT = 360 * 1000;
+    // 20s
+    public final static long CLEANER_SLEEP_TIMEOUT = 20 * 1000L;
     public final static String CRLF = "\r\n";
+    public final static String WWW_ROOT = "/home/lkm/www";
+    public final static String WWW_CLASSES = WWW_ROOT + "/WEB-INF/classes";
+    public final static String SERVER_NAME = "WSX";
+    public final static String SESSION_NAME = "JSESSIONID";
 }
