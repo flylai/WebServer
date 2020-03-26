3a824468afe826c69e723d92bbddaa5b294f6098
LKM <cnming2@qq.com>
2020-03-26 13:20:24 +0800

    [Session] add time counter

--- src/main/java/com/lkm/webserver/connection/Session.java
+++ src/main/java/com/lkm/webserver/connection/Session.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 
 public class Session {
     private HashMap<String, String> session;
+    private long lastUpdate;
 
     public Session(HashMap<String, String> session) {
         this.session = session;
+        updateLastUpdate();
     }
 
     public Session() {
@@ -15,13 +17,24 @@ public class Session {
 
     public void setAttribute(String key, String value) {
         session.put(key, value);
+        updateLastUpdate();
     }
 
     public String getAttribute(String key) {
+        updateLastUpdate();
         return session.get(key);
     }
 
     public void removeAttribute(String key) {
+        updateLastUpdate();
         session.remove(key);
+    }
+
+    private void updateLastUpdate() {
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    public long getLastUpdate() {
+        return lastUpdate;
     }
 }
