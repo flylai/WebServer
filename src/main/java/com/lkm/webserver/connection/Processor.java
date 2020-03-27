1091d2e7f4c8ca4404b374fb350ce24730a7ed53
LKM <cnming2@qq.com>
2020-03-27 16:07:39 +0800

    [Processor] add processor

---
+++ src/main/java/com/lkm/webserver/connection/Processor.java
@@ -0,0 +1,70 @@
+package com.lkm.webserver.connection;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.lkm.webserver.api.Servlet;
+import com.lkm.webserver.constant.Misc;
+import com.lkm.webserver.constant.RequestMethod;
+import com.lkm.webserver.exception.ContentLengthError;
+import com.lkm.webserver.request.Request;
+import com.lkm.webserver.request.RequestParser;
+import com.lkm.webserver.response.Response;
+import com.lkm.webserver.servlet.DefaultServlet;
+
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+import java.util.Map;
+import java.util.concurrent.*;
+
+public class Processor {
+    public static Map<String, Servlet> servletList;
+    private static final ThreadFactory threadFactory = new ThreadFactoryBuilder()
+            .setNameFormat("Request Processor Thread")
+            .build();
+    public static final ExecutorService requestExecutor =
+            new ThreadPoolExecutor(10,
+                    Misc.MAX_CONNECTION_POOL_SIZE,
+                    Misc.EXPIRES_TIME,
+                    TimeUnit.SECONDS,
+                    new LinkedBlockingQueue<>(),
+                    threadFactory);
+
+    public static void processRequest(Connection connection) {
+        if (connection == null || connection.isRunning() || connection.getHttpMessage().size() == 0) {
+            return;
+        }
+        connection.setRunning(true);
+        SocketChannel socketChannel = connection.getSocketChannel();
+        if (!socketChannel.isConnected()) {
+            return;
+        }
+
+        try {
+            Request request = RequestParser.parseMessage(connection);
+            connection.setRequest(request);
+            if (servletList.containsKey(request.getRequestLine().getPath())) {
+                Servlet servlet = servletList.get(request.getRequestLine().getPath());
+                if (request.getRequestLine().getMethod() == RequestMethod.GET) {
+                    servlet.doGet(request, new Response(socketChannel));
+                } else if (request.getRequestLine().getMethod() == RequestMethod.POST) {
+                    servlet.doPost(request, new Response(socketChannel));
+                }
+            } else {
+                DefaultServlet servlet = new DefaultServlet();
+                servlet.doGet(request, new Response(socketChannel));
+            }
+            connection.setRunning(false);
+            if (!"keep-alive".equals(request.getHeader("connection").toLowerCase())) {
+                ConnectionPool.removeSocket(socketChannel);
+            }
+        } catch (ContentLengthError e) {
+            e.printStackTrace();
+            try {
+                connection.getSocketChannel().close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
